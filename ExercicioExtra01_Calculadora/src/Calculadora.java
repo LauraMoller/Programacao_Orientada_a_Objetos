92b06bd0aaa96d525afbd862d4e21d39afacabe0
Laura Möller <163305319+LauraMoller@users.noreply.github.com>
2026-02-11 23:13:08 -0300

    Add files via upload

---
+++ ExercicioExtra01_Calculadora/src/Calculadora.java
@@ -0,0 +1,50 @@
+public class Calculadora {
+    private int n1;
+    private int n2;
+    private int resultado;
+
+    public int somar() {
+        setResultado(getN1() + getN2());
+        return getResultado();
+    }
+
+    public void subtrair(int n1, int n2) {
+        setResultado(n1 - n2);
+    }
+
+    public void dividir() {
+        setResultado(getN1() / getN2());
+    }
+
+    public int multiplicar(int n1, int n2) {
+        setResultado(n1 * n2);
+        return resultado;
+    }
+
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+
+    public void setN2(int n2) {
+        if (n2 == 0) {
+            throw new IllegalArgumentException("Numero 2 nao pode ser 0!");
+        }
+        this.n2 = n2;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public int getN1() {
+        return n1;
+    }
+
+    public int getN2() {
+        return n2;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
